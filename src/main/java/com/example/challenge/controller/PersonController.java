62ba9765fc4ba07e469ff1d7b979e56f4a3a9745
user <example@gmail.com>
2023-10-17 09:34:11 +0000

    added PersonController class

---
+++ src/main/java/com/example/challenge/controller/PersonController.java
@@ -0,0 +1,19 @@
+package com.example.challenge.controller;
+
+import com.example.challenge.model.Person;
+import com.example.challenge.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PersonController {
+    @Autowired
+    PersonRepository personRepository;
+    @GetMapping("/person")
+    public List<Person> getAllPerson(){
+        return personRepository.findAll();
+    }
+}
